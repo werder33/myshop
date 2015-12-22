093e6e31e9ec00836cb2cd77099e786475a6c849
werder33 <andreywow90@hmail.com>
2015-12-22 11:58:51 +0400

    test

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myshop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/myshop/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/myshop/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/myshop/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/myshop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/myshop/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/myshop/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/myshop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/myshop/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/myshop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/myshop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/myshop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1');
+define('DB_DATABASE', 'myshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
